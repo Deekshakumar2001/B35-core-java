41cead876a00f617dab934f605d6b4189d1111f3
Lenovo <Lenovo@Deeksha-Kumar.shrideviengineering.org>
2022-11-18 10:55:18 +0530

    first commit

---
+++ Day-1 connectingeclipsewithgithub/src/org/tnsindia/demo/Demoforgithubconnection.java
@@ -0,0 +1,10 @@
+package org.tnsindia.demo;
+
+public class Demoforgithubconnection {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to tns India foundation");
+
+	}
+
+}
