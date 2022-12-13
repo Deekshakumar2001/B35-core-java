a7735f2f849c4e852295a5059062fcc969059e88
Lenovo <Lenovo@Deeksha-Kumar>
2022-12-13 15:16:29 +0530

    first commit

---
+++ Day13-ExceptionHandling/src/exceptiondemo/ExecutorForThrow.java
@@ -0,0 +1,11 @@
+package exceptiondemo;
+//driver class
+public class ExecutorForThrow {
+
+	public static void main(String[] args) {
+		DemoonThrowkeyword d=new DemoonThrowkeyword();
+		d.display("Welcome to TNS India");
+
+	}
+
+}
