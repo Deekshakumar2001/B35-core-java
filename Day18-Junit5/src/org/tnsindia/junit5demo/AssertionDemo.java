90ec29b9f2924a5db3b22a270963e81339c9e374
Lenovo <Lenovo@Deeksha-Kumar>
2022-12-25 18:55:05 +0530

    firstcommit

---
+++ Day18-Junit5/src/org/tnsindia/junit5demo/AssertionDemo.java
@@ -0,0 +1,14 @@
+package org.tnsindia.junit5demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AssertionDemo {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Day18-Junit5/test/org/tnsindia/junitdemo/ExecutorForLifeCycleMethod.java
@@ -0,0 +1,19 @@
+package org.tnsindia.junitdemo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+
+public class ExecutorForLifeCycleMethod implements LifeCycleMethodDemo{
+	
+	@Test
+	//Declares a custom display name for the test class or test method
+	@DisplayName("Fast Method")
+	void fast()
+	{
+		System.out.println("First Test case");
+	}
+
+}
+
